fa2d6e3bc4ad1354335320bee617ce1832bef23e
Aniruddha Kulkarni <aniruddha.k1911@gmail.com>
2021-06-06 15:52:50 +0530

    Update settings.py

--- API/settings.py
+++ API/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
     'rest_framework',
     'corsheaders',
     'alerts.apps.AlertsConfig',
