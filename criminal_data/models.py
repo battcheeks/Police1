3df860ed8a3f03b0e7a14ea329c92426b381f3c0
Aniruddha Kulkarni <aniruddha.k1911@gmail.com>
2021-06-11 16:07:56 +0530

    Update models.py

--- criminal_data/models.py
+++ criminal_data/models.py
@@ -6,16 +6,16 @@ from multiselectfield import MultiSelectField
 
 class CriminalDataAPI(models.Model):
     name = models.CharField(max_length=100, blank=False, default='')
-    dob = models.DateField()
-    known_crimes = models.TextField()
+    dob = models.DateField(blank=True)
+    known_crimes = models.TextField(blank=True)
     REGISTER_CHOICES = (('Y', 'Yes'), ('N', 'No'),)
     registered = models.CharField(max_length=1, choices=REGISTER_CHOICES)
     registered_at = models.CharField(max_length=100, blank=False, default='')
     CUSTODY_CHOICES = (('Y', 'Yes'), ('N', 'No'),)
     custody = models.CharField(max_length=1, choices=CUSTODY_CHOICES)
-    latest_sightings_time = models.TimeField()
-    latest_sightings_date = models.DateField()
-    latest_sightings_place = models.CharField(max_length=100, blank=False, default='')
+    latest_sightings_time = models.TimeField(blank=True)
+    latest_sightings_date = models.DateField(blank=True)
+    latest_sightings_place = models.CharField(max_length=100, blank=True, default='')
 
 # class Meta:
 #     ordering = ['appointment_date']
