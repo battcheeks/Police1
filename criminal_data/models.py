b767d1133b4378ec830047336d5301a270e6d8a0
Aniruddha Kulkarni <aniruddha.k1911@gmail.com>
2021-06-12 03:05:02 +0530

    Update models.py

--- criminal_data/models.py
+++ criminal_data/models.py
@@ -5,17 +5,17 @@ from multiselectfield import MultiSelectField
 
 
 class CriminalDataAPI(models.Model):
-    name = models.CharField(max_length=100, blank=True, default='')
-    dob = models.DateField(blank=True,null=True)
-    known_crimes = models.TextField(blank=True)
+    name = models.CharField(max_length=100, default='')
+    dob = models.DateField()
+    known_crimes = models.TextField()
     REGISTER_CHOICES = (('Y', 'Yes'), ('N', 'No'),)
     registered = models.CharField(max_length=1, choices=REGISTER_CHOICES)
-    registered_at = models.CharField(max_length=100, blank=False, default='')
+    registered_at = models.CharField(max_length=100, default='')
     CUSTODY_CHOICES = (('Y', 'Yes'), ('N', 'No'),)
     custody = models.CharField(max_length=1, choices=CUSTODY_CHOICES)
-    latest_sightings_time = models.TimeField(blank=True,null=True)
-    latest_sightings_date = models.DateField(blank=True,null=True)
-    latest_sightings_place = models.CharField(max_length=100, blank=True, null=True)
+    latest_sightings_time = models.TimeField()
+    latest_sightings_date = models.DateField()
+    latest_sightings_place = models.CharField(max_length=100)
 
 # class Meta:
 #     ordering = ['appointment_date']
